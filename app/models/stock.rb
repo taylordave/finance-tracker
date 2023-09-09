a054c6acdaf5bc3c60ebd69c1df135eb83a81e8d
Dave Taylor <ddataylor@gmail.com>
2023-09-09 12:49:01 +0100

    Create and display objects in browser

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -6,7 +6,10 @@ class Stock < ApplicationRecord
       secret_token: Rails.application.credentials.iex[:secret_key],
       endpoint: 'https://cloud.iexapis.com/v1'
     )
-    client.price(ticker_symbol)
+    new(ticker: ticker_symbol,
+        name: client.company(ticker_symbol).company_name,
+        last_price: client.price(ticker_symbol)
+    )
   end
 
 end
