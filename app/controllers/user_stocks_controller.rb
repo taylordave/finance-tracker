0cad1f2b09c9dd151ba455c2b3832ba5a30d0114
Dave Taylor <ddataylor@gmail.com>
2023-09-10 14:45:02 +0100

    Track stocks from front-end browser

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -1,2 +1,14 @@
 class UserStocksController < ApplicationController
+
+  def create
+    stock = Stock.find_by_ticker(params[:ticker])
+    if stock.blank?
+      stock = Stock.new_lookup(params[:ticker])
+      stock.save
+    end
+    @user_stock = UserStock.create(user: current_user, stock: stock)
+    flash[:notice] = "Stock #{stock.name} was successfully added to your portfolio"
+    redirect_to my_portfolio_path
+  end
+
 end
