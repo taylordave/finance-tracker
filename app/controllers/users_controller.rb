8c16eb1ab952dd8f191b2c577b95d7ef9a1eed6e
Dave Taylor <ddataylor@gmail.com>
2023-09-10 21:26:50 +0100

    Add JavaScript response to form submission

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,24 @@ class UsersController < ApplicationController
   end
 
   def search
-    render json: params[:friend]
+    if params[:friend].present?
+      @friend = params[:friend]
+      if @friend
+        respond_to do |format|
+          format.js { render partial: 'users/friend_result' }
+        end
+      else
+        respond_to do |format|
+          flash.now[:alert] = "Please enter a valid symbol to search"
+          format.js { render partial: 'users/friend_result' }
+        end
+      end
+    else
+      respond_to do |format|
+        flash.now[:alert] = "Please enter a friend name or email to seaerch"
+        format.js { render partial: 'users/friend_result' }
+      end
+    end
   end
 
 end
